76822b15df4c79aaad68d219d9d3f9e12b7dbdf1
Nick-2k1 <nick@ordernow.au>
2026-07-03 10:54:41 +0700

    feat: add some generate string utils

---
+++ projects/core/src/lib/string-handler/generate-random-string.ts
@@ -0,0 +1,6 @@
+export function generateRandomString(length: number = 16): string {
+  const bytes = new Uint8Array(length);
+  crypto.getRandomValues(bytes);
+
+  return Array.from(bytes, (b) => b.toString(16).padStart(2, '0')).join('');
+}
--- projects/core/src/lib/string-handler/index.ts
+++ projects/core/src/lib/string-handler/index.ts
@@ -1,3 +1,5 @@
 export { capitalizeFirst, capitalizeTextFields, capitalizeWords } from './capitalize-text';
 export { generateHash } from './generate-hash';
+export { generateId } from './generate-id';
+export { generateRandomString } from './generate-random-string';
 export { removeTones } from './remove-tones';
