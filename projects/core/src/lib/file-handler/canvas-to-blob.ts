f067eb2f38c3f42fe04864e8b577e3ee7ebbf8ce
Nick-2k1 <nick@ordernow.au>
2026-07-03 15:10:11 +0700

    feat: add compress-image-file utils

---
+++ projects/core/src/lib/file-handler/canvas-to-blob.ts
@@ -0,0 +1,22 @@
+import { CanvasImageFormat } from './canvas.type';
+
+export function canvasToBlob(
+  canvas: HTMLCanvasElement,
+  outputFormat: CanvasImageFormat,
+  quality: number,
+): Promise<Blob> {
+  return new Promise((resolve, reject) => {
+    canvas.toBlob(
+      (blob) => {
+        if (!blob) {
+          reject(new Error('Compression failed'));
+          return;
+        }
+
+        resolve(blob);
+      },
+      outputFormat,
+      quality,
+    );
+  });
+}
---
+++ projects/core/src/lib/file-handler/compress-image/compress-image-file.ts
@@ -0,0 +1,44 @@
+import { canvasToBlob } from '../canvas-to-blob';
+import { CanvasImageFormat, canvasQualityByFormat } from '../canvas.type';
+import { fileToDataUrl } from '../file-to-data-url';
+import { loadImage } from '../load-image';
+
+export async function compressImageFile(
+  file: File,
+  config?: {
+    maxWidth?: number;
+    outputFormat?: CanvasImageFormat;
+    quality?: number;
+  },
+): Promise<File> {
+  const { maxWidth = 1024, outputFormat = 'image/jpeg' } = config ?? {};
+  const quality = config?.quality ?? canvasQualityByFormat[outputFormat];
+
+  const dataUrl = await fileToDataUrl(file);
+  const img = await loadImage(dataUrl);
+
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  if (!ctx) {
+    throw new Error('Canvas not supported');
+  }
+
+  let { width, height } = img;
+  if (width > maxWidth) {
+    const scale = maxWidth / width;
+    width = maxWidth;
+    height = height * scale;
+  }
+
+  canvas.width = width;
+  canvas.height = height;
+
+  ctx.drawImage(img, 0, 0, width, height);
+
+  const blob = await canvasToBlob(canvas, outputFormat, quality);
+
+  return new File([blob], file.name, {
+    type: outputFormat,
+  });
+}
