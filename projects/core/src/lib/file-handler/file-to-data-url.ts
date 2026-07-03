46c3ef40625337cfe836dc539f98b9be4411de8d
Nick-2k1 <nick@ordernow.au>
2026-07-03 15:07:54 +0700

    feat: add file-to-data-url util

---
+++ projects/core/src/lib/file-handler/file-to-data-url.ts
@@ -0,0 +1,10 @@
+export function fileToDataUrl(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = () => resolve(reader.result as string);
+    reader.onerror = (error) => reject(error);
+
+    reader.readAsDataURL(file);
+  });
+}
