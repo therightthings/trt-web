42793a4460841a4adae473b8ad18b5b6618963b4
namnguyen2k1 <nanam133hg@gmail.com>
2026-07-04 18:02:36 +0700

    feat: update export

--- projects/core/src/lib/string-handler/index.ts
+++ projects/core/src/lib/string-handler/index.ts
@@ -3,3 +3,4 @@ export { generateHash } from './generate-hash';
 export { generateId } from './generate-id';
 export { generateRandomString } from './generate-random-string';
 export { removeTones } from './remove-tones/remove-tones';
+export { generateSearchKeys } from './search-key/generate-search-key';
