259dcb2e38381b4389c10d06e348b6ecb6e3cc7d
namnguyen2k1 <nanam133hg@gmail.com>
2026-07-08 23:59:12 +0700

    feat(firebase-admin): create worker utils

--- projects/core/src/lib/browser/index.ts
+++ projects/core/src/lib/browser/index.ts
@@ -1,3 +1,5 @@
 export { LocalStorage } from './local-storage/local-storage';
 export { BrowserPermission } from './permission/browser-permission';
 export { SessionStorage } from './session-storage/session-storage';
+export { createWorker } from './worker/create-worker';
+export { runWorker } from './worker/run-worker';
