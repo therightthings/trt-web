9a888dba114d94f613be3addb5a0dc656d63a4eb
namnguyen2k1 <nanam133hg@gmail.com>
2026-07-13 15:59:36 +0700

    chore(react-demo): update eslint config

--- projects/react-demo/eslint.config.mjs
+++ projects/react-demo/eslint.config.mjs
@@ -1,10 +1,22 @@
 import nx from '@nx/eslint-plugin';
+import tseslint from 'typescript-eslint';
 
 export const reactDemoConfig = [
-  ...nx.configs['flat/react'],
+  ...nx.configs['flat/react-base'],
+  ...nx.configs['flat/react-jsx'],
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    // Override or add rules here
+    files: ['**/*.{ts,tsx,cts,mts,js,jsx,cjs,mjs}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
     rules: {},
   },
 ];
