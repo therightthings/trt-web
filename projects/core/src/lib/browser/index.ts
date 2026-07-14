05a2f78119fe259fcbc98ae3b5ba31f1d9aa061a
namnguyen2k1 <nanam133hg@gmail.com>
2026-07-14 22:21:22 +0700

    chore(core): export namespace + static class

--- projects/core/src/lib/browser/index.ts
+++ projects/core/src/lib/browser/index.ts
@@ -1,12 +1,2 @@
-export { BrowserClipboard } from './clipboard/browser-clipboard';
-export { Cookie } from './cookie/cookie';
-export { BrowserEnvironment } from './environment/browser-environment';
-export { LocalStorage } from './local-storage/local-storage';
-export { BrowserLocation } from './location/browser-location';
-export { BrowserPermission } from './permission/browser-permission';
-export { BrowserPresentation } from './presentation/browser-presentation';
-export { BrowserResource } from './resource/browser-resource';
-export { SessionStorage } from './session-storage/session-storage';
-export { BrowserShare } from './share/browser-share';
 export { createWorker } from './worker/create-worker';
 export { runWorker } from './worker/run-worker';
